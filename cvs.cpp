52e62d85f0c56df2aa530654e828bc975cc35797
Gustavo Rubio <grubio3796@gmail.com>
2021-04-27 20:15:21 -0700

    Complete CSV output files (TODO: misspelled file names)

--- cvs.cpp
+++ cvs.cpp
@@ -6,24 +6,25 @@
 
 using namespace std;
 
-int output() {
-    cout << "Please enter the name of the new file being created (without the .csv):" << endl;
-    string OUTPUT;
-    cin >> OUTPUT;
-    // Create an output filestream object
-    OUTPUT = OUTPUT + ".csv";
-    std::ofstream myFile(OUTPUT);
-    
-    
+void CSV::outputCSV(string fileInfo1,string fileInfo2,string fileInfo3,string fileInfo4){
+  cout << "Please enter the name of the new file being created (without the .csv):" << endl;
+  string OUTPUT;
+  cin >> OUTPUT;
+  // Create an output filestream object
+  OUTPUT = OUTPUT + ".csv";
+  std::ofstream myFile(OUTPUT);
 
-    // Send data to the stream
-    myFile << ",Artist,Track,Album,Data,Genre,Track Number,Comments,";
-    myFile << "\n1\n";
-    myFile << "2\n";
-    myFile << "3\n";
-    
-    // Close the file
-    myFile.close();
-    
-    return 0;
+  // Send data to the stream
+  myFile << ",Artist,Track,Album,Data,Genre,Track Number,Comments,";
+  myFile << "\n";
+  myFile << fileInfo1;
+  myFile << "\n";
+  myFile << fileInfo2;
+  myFile << "\n";
+  myFile << fileInfo3;
+  myFile << "\n";
+  myFile << fileInfo4;
+  
+  // Close the file
+  myFile.close();
 }
--- cvs.h
+++ cvs.h
@@ -3,3 +3,9 @@
 #include <string>
 
 using namespace std;
+
+class CSV{
+  public:
+    void outputCSV(string fileInfo1,string fileInfo2,string fileInfo3,string fileInfo4);
+    void printprint();
+};
