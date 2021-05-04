8668d4e70c261aeda8a6d363141a79a096070a74
Tony Lang <TonylsLame@users.noreply.github.com>
2021-05-03 20:00:50 -0700

    Checks for file name before saving a file

--- main.cpp
+++ main.cpp
@@ -37,47 +37,84 @@ void fn(){
 
 int main(){
 	
-  std::vector<std::string> fileList;  
-  struct dirent *directory;
-  DIR *wav_dr;
-  wav_dr = opendir("./wav_folder");
+	std::vector<std::string> fileList;
+	std::ofstream outFile;
+	std::string userFileName;
+	bool same = true;
 
-  if(wav_dr!=NULL)
-  {
-    for(directory=readdir(wav_dr); directory!=NULL; directory=readdir(wav_dr))
-      fileList.push_back(directory->d_name);
+	struct dirent *directory;
+	DIR *wav_dr;
+	wav_dr = opendir("./wav_folder");
 
-    closedir(wav_dr);
+	if(wav_dr!=NULL)
+	{
+    	for(directory=readdir(wav_dr); directory!=NULL; directory=readdir(wav_dr))
+    	{
+      		fileList.push_back(directory->d_name); // contains fileNames (maybe not necessary)
+    	}
+    	closedir(wav_dr);
 
-    for (int i = 2; i < fileList.size(); i++)
-    {  
-      std::cout << fileList.at(i) << std::endl; // first two entries are junk, start at i = 2
-    }
-}
-else
-  std::cout<<"\nEncountered an error";
-std::cout << std::endl;
-
-
-WavReader yourMom;
-yourMom.readFile("yes-16-bit-mono.wav");
+    	//mainly for testing; outputs names of files read in
+	    for (int i = 0; i < fileList.size(); i++)
+	    {  
+	      std::cout << fileList.at(i) << std::endl; // first two entries are junk, start at i = 2
+	    } // random file ".DS_Store" is being created
 
 
+  	}
+  	else
+	    std::cout<<"\nEncountered an error";
 
 
 
-  //TODO: Call a method that calls all the getters for the meta/technical data, compose into one string for each audio file
-  //TODO: Replace "eat,drink,sleep,repeat" with data strings
-string fileInfo1 = "eat,drink,sleep,repeat";
-string fileInfo2 = "eat,drink,sleep,repeat";
-string fileInfo3 = "eat,drink,sleep,repeat";
-string fileInfo4 = "eat,drink,sleep,repeat";
-  /*
-  CSV* csv;
-  cout << "Please enter the name of the new file being created (without the .csv):" << endl;
-  string OUTPUT;
-  cin >> OUTPUT;
-  csv->outputCSV(OUTPUT,fileInfo1, fileInfo2, fileInfo3, fileInfo4);
+
+	// files processed by this point
+	do
+	{
+		std::cout << "Enter the name you want the new file to be called.\nName: ";
+		std::cin >> userFileName;
+
+		for (int n = 2; n < fileList.size(); n++)
+		{
+			if (fileList.at(n).compare(userFileName) == 0)
+			{
+				std::cout << "Files cannot be saved with the same name as an extisting file." << std::endl;
+				same = true;
+				n = fileList.size();
+			}
+			else
+				same = false;
+		}
+	} while (same == true);
+
+	outFile.open("./Output_Folder/"+userFileName);
+
+	for (int n = 2; n < fileList.size(); n++)
+	{
+		outFile << fileList.at(n) << std::endl;
+	}
+
+	outFile.close();
+
+	WavReader yourMom;
+	yourMom.readFile("yes-16-bit-mono.wav");
+
+
+
+
+
+	//TODO: Call a method that calls all the getters for the meta/technical data, compose into one string for each audio file
+	//TODO: Replace "eat,drink,sleep,repeat" with data strings
+	string fileInfo1 = "eat,drink,sleep,repeat";
+	string fileInfo2 = "eat,drink,sleep,repeat";
+	string fileInfo3 = "eat,drink,sleep,repeat";
+	string fileInfo4 = "eat,drink,sleep,repeat";
+	/*
+	CSV* csv;
+	cout << "Please enter the name of the new file being created (without the .csv):" << endl;
+	string OUTPUT;
+	cin >> OUTPUT;
+	csv->outputCSV(OUTPUT,fileInfo1, fileInfo2, fileInfo3, fileInfo4);
 	*/
-return 0;
+	return 0;
 }
