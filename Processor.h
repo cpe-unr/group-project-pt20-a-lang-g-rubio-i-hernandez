254aec1851eb8e6b1dd0a0f17a3b5ab37ac47f04
Gustavo-Rubio <70547757+Gustavo-Rubio@users.noreply.github.com>
2021-05-04 22:23:15 -0700

    Update Processor with doxygen

--- Processor.h
+++ Processor.h
@@ -8,6 +8,11 @@
 template<class T>
 class Processor {
 public:
+    /**
+    *Edits the audio somehow; inplemented in subclasses
+    *@param buffer - the actual audio
+    *@param bufferSize - the length of this audio
+    */
     virtual void processBuffer<T>(T* buffer, int bufferSize) = 0;
 };
 
