a720c56886144aee69bed0d616c910bb8ab53376
Gustavo-Rubio <70547757+Gustavo-Rubio@users.noreply.github.com>
2021-05-04 22:21:48 -0700

    Update NoiseGate with doxygen

--- NoiseGate.h
+++ NoiseGate.h
@@ -10,7 +10,9 @@ class NoiseGate : public Processor<T> {
 public:
 /**
 *Sets the audio equal to 'zero' (silence) if it's not loud enough
-*/
+    *@param buffer - the actual audio
+    *@param bufferSize - the length of this audio
+    */
 	void processBuffer(T* buffer, int bufferSize)override{
     const T ZERO = (pow(2, bitType)/2);
     const T threshold = ZERO + (ZERO * .05)
